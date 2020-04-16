8ad19d4c9e0e84c52e0e127650a1c70aa3f2b76c
achianumba <chianumbaarinze@gmail.com>
2020-04-16 17:17:12 +0100

    1.4 course info step4: using arrays as props

--- part1/course-information/src/index.js
+++ part1/course-information/src/index.js
@@ -8,32 +8,26 @@ App
 const App = () => {
   const course = "Half Stack application development";
 
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1.name}
-        exercises1={part1.exercises}
-        part2={part2.name}
-        exercises2={part2.exercises}
-        part3={part3.name}
-        exercises3={part3.exercises}
-      />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -44,18 +38,11 @@ Header
 */
 const Header = ({ course }) => <h1>{course}</h1>;
 
-const Content = ({
-  part1,
-  part2,
-  part3,
-  exercises1,
-  exercises2,
-  exercises3,
-}) => (
+const Content = ({ parts }) => (
   <>
-    <Part part={part1} exercises={exercises1} />
-    <Part part={part2} exercises={exercises2} />
-    <Part part={part3} exercises={exercises3} />
+    <Part part={parts[0].name} exercises={parts[0].exercises} />
+    <Part part={parts[1].name} exercises={parts[1].exercises} />
+    <Part part={parts[2].name} exercises={parts[2].exercises} />
   </>
 );
 /*
@@ -73,6 +60,8 @@ const Part = ({ part, exercises }) => (
 Total
 =====================
 */
-const Total = ({ total }) => <p>Number of exercises {total}</p>;
+const Total = ({ parts }) => <p>
+Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+</p>;
 
 ReactDOM.render(<App />, document.getElementById("root"));
