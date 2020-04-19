ea5b3b573c3eefec546bae6ebd43b114fde3cce8
achianumba <chianumbaarinze@gmail.com>
2020-04-19 20:03:31 +0100

    2.1: course contents step6

---
+++ part2/course-contents/README.md
@@ -0,0 +1,13 @@
+# Course-Contents
+
+This directory contains my solutions to the _course-contents_ exercises in _part2_ of _FullstackOpen-2020_ web development course. It's builds upon the excercises in _part1's course-info_.
+
+This project is built with React using Create-React-App.
+
+1. To test it out on your local machine, simply download all files and folders in this directory or the whole repo, and extract this directory from the zip archive if you've downloaded the entire repo.
+
+2. Fire up your terminal and change the current working directory to the folder containing the extracted files.
+
+3. Type `npm install` and press `Enter` to install dependencies for this project.
+
+4. Type `npm start` and press `Enter` to start the development server and view the project in the browser.
---
+++ part2/course-contents/src/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+/*
+=====================
+App
+=====================
+*/
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      }
+    ],
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+/*
+=====================
+Course
+=====================
+*/
+const Course = ({ course }) => {
+  return (
+    <div className="course">
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+/*
+=====================
+Header
+=====================
+*/
+const Header = ({ name }) => <h1 className="header">{name}</h1>;
+
+const Content = ({ parts }) => (
+  <>
+  {
+    parts.map(({ id, name, exercises}) => <Part key={ id } part={name} exercises={exercises} />)
+  }
+  </>
+);
+/*
+=====================
+Part
+=====================
+*/
+const Part = ({ part, exercises }) => (
+  <p className="part">
+    {part} {exercises}
+  </p>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
