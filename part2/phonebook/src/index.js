72753149abc8367916d69ba7fe22f5235fcf38d5
achianumba <chianumbaarinze@gmail.com>
2020-04-30 21:37:58 +0100

    exercise 2.11: fetching contacts from json server

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,18 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import axios from 'axios';
 /*
 =====================
 Phonebook
 =====================
 */
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -45,6 +41,11 @@ const App = () => {
     );
     setResults(results);
   };
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => setPersons(response.data));
+  }, []);
 
   return (
     <div id="phonebook">
